48ddaa10fe9cf404c0b85b3278512fbe4fedafb1
Reuben Dunnington <rdunnington@believer.gg>
2024-06-17 15:58:13 -0700

    fix: OFPA name translation Friendshipper compat * Add a ListFile param to support passing filename translation requests via file instead of commandline. Often OFPA workloads generate more paths than the Windows commandline length limits can handle.

--- Source/FriendshipperCore/Private/FriendshipperTranslateOFPAFilenamesCommandlet.h
+++ Source/FriendshipperCore/Private/FriendshipperTranslateOFPAFilenamesCommandlet.h
@@ -9,10 +9,14 @@
 //
 // Usage:
 //
-//   UnrealEditor-Cmd.exe <PathToUProject> -run=TranslateOFPAFilenames <Space separated filenames.>
 //
+//   UnrealEditor-Cmd.exe <PathToUProject> -run=TranslateOFPAFilenames [-ListFile=<Path/to/file>] [Space separated filenames]
 // Arguments:
-//		Paths to assets to translate. Specify a space-separated list of paths. For example:
+//     Listfile (optional): Instead of looking for names of paths as arguments to the commandlet, reads a file for a
+//       newline-separated list of paths. This option is provided to get around the commandline length limits. If this
+//       option is specified, only filenames given in the listfile will be translated.
+//
+//	   Paths to assets to translate. Specify a space-separated list of paths. For example:
 //       -run=TranslateOFPAFilenames D:/repos/fellowship/Plugins/GameFeatures/ShooterMaps/Content/__ExternalActors__/Maps/L_Convolution_Blockout/0/CR/ZZ6IFPOFLPOW1WBSFYOPW6.uasset
 //       -run=TranslateOFPAFilenames D:/repos/fellowship/Content/__ExternalObjects__/Levels/DevTest/Traversal/L_TraversalGym/0/Z0/ZZGHJ5DWZEAJAM6BNQYL8O.uasset
 //       -run=TranslateOFPAFilenames Content/__ExternalObjects__/Levels/DevTest/Traversal/L_TraversalGym/0/Z0/ZZGHJ5DWZEAJAM6BNQYL8O.uasset Content/__ExternalActors__/Levels/DevTest/Combat/L_CombatGym/C/BO/ZZGR76KX8SMOWJNZAZ63KX.uasset
