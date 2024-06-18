03d0787582892b2004bd3a752c6014f2d2936642
Reuben Dunnington <4075241+rdunnington@users.noreply.github.com>
2024-06-18 13:55:38 -0700

    fix: OFPA name translation Friendshipper compat (#3)
    * Add a ListFile param to support passing filename translation requests via file instead of commandline. Often OFPA workloads generate more paths than the Windows commandline length limits can handle.

--- Source/FriendshipperCore/Private/FriendshipperTranslateOFPAFilenamesCommandlet.cpp
+++ Source/FriendshipperCore/Private/FriendshipperTranslateOFPAFilenamesCommandlet.cpp
@@ -13,6 +13,35 @@ int32 UTranslateOFPAFilenamesCommandlet::Main(const FString& Params)
 
 	UCommandlet::ParseCommandLine(*Params, Tokens, Switches, SwitchParams);
 
+	if (FString* ListFilePath = SwitchParams.Find(TEXT("ListFile")))
+	{
+		Tokens.Reset();
+
+		// Trim surrounding single quotes if present. Friendshipper passes this string in single quotes to safely
+		// handle spaces in the path.
+		if (ListFilePath->StartsWith("'"))
+		{
+			ListFilePath->RightChopInline(1, EAllowShrinking::No);
+		}
+
+		if (ListFilePath->EndsWith("'"))
+		{
+			ListFilePath->LeftChopInline(1, EAllowShrinking::No);
+		}
+
+		FString FileContents;
+		if (FFileHelper::LoadFileToString(FileContents, **ListFilePath) == false)
+		{
+			UE_LOG(LogFriendshipperTranslateOFPAFilenamesCommandlet, Error,
+				TEXT("Unable to find provided ListFile '%s'. Unable to translate filenames."),
+				**ListFilePath);
+			return 1;
+		}
+
+		bool bInCullEmpty = true;
+		FileContents.ParseIntoArray(Tokens, TEXT("\n"), bInCullEmpty); // friendshipper always writes newlines only
+	}
+
 	TArray<FAssetFriendlyName> FriendlyNames = OfpaUtils::TranslatePackagePaths(Tokens); // We expect Tokens to be FilePaths
 
 	for (const FAssetFriendlyName& FriendlyName : FriendlyNames)
